5ba1d1508fd975538e3305dc0bcf43afc1afd249
Htet <kiasuwhite@hotmail.com>
2021-07-21 14:08:36 +0800

    Add mixed messages script

---
+++ mixed_messages.js
@@ -0,0 +1,12 @@
+function generateRandomNumber(num) {
+    // Gets # from 0 -> num - 1
+    return Math.floor(Math.random() * num)
+  }
+
+pokemon = ['Treecko','Grovyle','Sceptile','Torchic','Combusken','Blaziken','Mudkip','Marshtomp','Swampert','Poochyena','Mightyena','Zigzagoon','Linoone','Wurmple','Silcoon','Beautifly','Cascoon','Dustox','Lotad','Lombre','Ludicolo','Seedot','Nuzleaf','Shiftry','Taillow','Swellow','Wingull','Pelipper','Ralts','Kirlia','Gardevoir','Surskit','Masquerain','Shroomish','Breloom','Slakoth','Vigoroth','Slaking','Nincada','Ninjask','Shedinja','Whismur','Loudred','Exploud','Makuhita','Hariyama','Azurill','Nosepass','Skitty','Delcatty','Sableye','Mawile','Aron','Lairon','Aggron','Meditite','Medicham','Electrike','Manectric','Plusle','Minun','Volbeat','Illumise','Roselia','Gulpin','Swalot','Carvanha','Sharpedo','Wailmer','Wailord','Numel','Camerupt','Torkoal','Spoink','Grumpig','Spinda','Trapinch','Vibrava','Flygon','Cacnea','Cacturne','Swablu','Altaria','Zangoose','Seviper','Lunatone','Solrock','Barboach','Whiscash','Corphish','Crawdaunt','Baltoy','Claydol','Lileep','Cradily','Anorith','Armaldo','Feebas','Milotic','Castform','Kecleon','Shuppet','Banette','Duskull','Dusclops','Tropius','Chimecho','Absol','Wynaut','Snorunt','Glalie','Spheal','Sealeo','Walrein','Clamperl','Huntail','Gorebyss','Relicanth','Luvdisc','Bagon','Shelgon','Salamence','Beldum','Metang','Metagross','Regirock','Regice','Registeel','Latias','Latios','Kyogre','Groudon','Rayquaza','Jirachi','Deoxys']
+city = ['Littleroot Town', 'Oldale Town', 'Petalburg City', 'Rustboro City', 'Dewford Town', 'Slateport City', 'Mauville City', 'Verdanturf Town', 'Fallarbor Town', 'Lavaridge Town', 'Fortree City', 'Lilycove City', 'Mossdeep City', 'Sootopolis City', 'Pacifidlog Town', 'Ever Grande City']
+attempts = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(`Your hometown is in ${city[generateRandomNumber(city.length)]}.`);
+console.log(`Your favorite pokemon is ${pokemon[generateRandomNumber(pokemon.length)]}.`);
+console.log(`You will take ${attempts[generateRandomNumber(attempts.length)]} attempts before you finally beat the Elite Four!`);
